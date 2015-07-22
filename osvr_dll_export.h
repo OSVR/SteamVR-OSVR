a15bc1f09f4ee35284d47c467015e9b24c90e1df
Ryan Pavlik <ryan@sensics.com>
2015-07-22 13:16:21 -0500

    Clean up the dll export header.

--- osvr_dll_export.h
+++ osvr_dll_export.h
@@ -25,15 +25,6 @@
 #ifndef INCLUDED_osvr_dll_export_h_GUID_86142D80_1F9B_45D5_9C6C_04FA424822B0
 #define INCLUDED_osvr_dll_export_h_GUID_86142D80_1F9B_45D5_9C6C_04FA424822B0
 
-// Internal Includes
-// - none
-
-// Library/third-party includes
-// - none
-
-// Standard includes
-// - none
-
 #if defined(_WIN32)
 #define OSVR_DLL_EXPORT extern "C" __declspec(dllexport)
 #define OSVR_DLL_IMPORT extern "C" __declspec(dllimport)
