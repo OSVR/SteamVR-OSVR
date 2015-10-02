0e012d19db6c6d9b92568517d4fc84f94005ed8a
David Ryskalczyk <d235j.1@gmail.com>
2015-10-01 23:12:22 -0400

    Fix build with OpenVR 0.9.9 and Mac build

--- osvr_dll_export.h
+++ osvr_dll_export.h
@@ -28,7 +28,7 @@
 #if defined(_WIN32)
 #define OSVR_DLL_EXPORT extern "C" __declspec(dllexport)
 #define OSVR_DLL_IMPORT extern "C" __declspec(dllimport)
-#elif defined(GNUC) || defined(COMPILER_GCC)
+#elif defined(GNUC) || defined(COMPILER_GCC) || defined(__APPLE__)
 #define OSVR_DLL_EXPORT extern "C" __attribute__((visibility("default")))
 #define OSVR_DLL_IMPORT extern "C"
 #else
