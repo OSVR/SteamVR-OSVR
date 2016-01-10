e6bf54c8c45929429be15e83b63eff095098f42c
Kevin M. Godby <kevin@godby.org>
2016-01-09 23:42:09 -0600

    Forgot to add sudo to the make install command for Travis CI.

--- devtools/travis-install.sh
+++ devtools/travis-install.sh
@@ -20,21 +20,21 @@ if [ $TRAVIS_OS_NAME = 'linux' ]; then
     mkdir $HOME/jsoncpp/build
     pushd $HOME/jsoncpp/build
     cmake .. -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=ON -DJSONCPP_WITH_CMAKE_PACKAGE=ON -DCMAKE_BUILD_TYPE=${CONFIG}
-    make -j2 install
+    sudo make -j2 install
     popd
 
     # Build libfunctionality
     mkdir -p $HOME/libfunctionality/build
     pushd $HOME/libfunctionality/build
     cmake .. -DCMAKE_BUILD_TYPE=${CONFIG}
-    make -j2 install
+    sudo make -j2 install
     popd
 
     # Build OSVR-Core
     mkdir -p $HOME/OSVR-Core/build
     pushd $HOME/OSVR-Core/build
     cmake .. -DCMAKE_BUILD_TYPE=${CONFIG} -DCMAKE_PREFIX_PATH=/usr/local
-    make -j2 install
+    sudo make -j2 install
     popd
 fi
 
