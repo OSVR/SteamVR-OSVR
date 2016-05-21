0b3e8f11689a3bee15dcebf72df7fccb70a70861
Kevin M. Godby <kevin@godby.org>
2016-05-20 22:55:46 -0500

    Adjusted logger.

--- src/Logging.h
+++ src/Logging.h
@@ -78,18 +78,31 @@ public:
         // do nothing
     }
 
+    ~LineLogger()
+    {
+        // Log the queued message
+        driverLog_->Log(stream_->str().c_str());
+    }
+
+    LineLogger(LineLogger&& other)
+    {
+        using std::swap;
+        severity_ = other.severity_;
+        driverLog_ = other.driverLog_;
+        swap(stream_, other.stream_);
+    }
+
     template <typename T>
     LineLogger& operator<<(const T& msg)
     {
-        std::ostringstream oss;
-        oss << msg;
-        driverLog_->Log(oss.str().c_str());
+        *stream_ << msg;
         return *this;
     }
 
 protected:
     LogLevel severity_ = LogLevel::info;
     vr::IDriverLog* driverLog_;
+    std::unique_ptr<std::ostringstream> stream_;
 };
 
 /**
@@ -148,7 +161,7 @@ public:
         return severity_;
     }
 
-    LineLogger log(LogLevel severity)
+    LineLogger& log(LogLevel severity)
     {
         if (severity >= severity_) {
             LineLogger logger(severity, driverLog_);
